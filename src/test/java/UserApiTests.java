b6d691b41c49f5d9fe315b7f6fdf00041e6381e9
Alex <udinkik@mail.ru>
2025-05-11 19:31:11 +0300

    add getUser

--- src/test/java/UserApiTests.java
+++ src/test/java/UserApiTests.java
@@ -1,17 +1,18 @@
 import io.restassured.http.ContentType;
-import io.restassured.mapper.ObjectMapperType;
 import models.JwtAuth;
 import models.User;
 import models.info.ErrorInfoAuth;
 import models.info.Info;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ApiTest extends BaseApiTest {
+@Tag("api")
+public class UserApiTests extends BaseApiTest {
 
     private static final String USER_CREATED_MESSAGE = "User created";
     private static final String USER_FAIL_CREATED_MESSAGE = "Login already exist";
@@ -20,30 +21,39 @@ public class ApiTest extends BaseApiTest {
     private static final String ERROR_UNAUTHORIZED = "Unauthorized";
 
     @Test
+    @Tag("positiveUserTest")
     @DisplayName("Позитивная регистрация пользователя")
     public void positiveRegisterUser() {
+
         int randomNumber = Math.abs(random.nextInt());
         User user = User.builder().login("alex" + randomNumber).pass("human" + randomNumber).build();
+
         Info info = given().contentType(ContentType.JSON)
                 .body(user)
                 .post("/api/signup")
                 .then()
+                .statusCode(201)
                 .extract()
                 .jsonPath().getObject("info", Info.class);
+
         assertEquals(USER_CREATED_MESSAGE, info.getMessage());
         assertEquals(USER_CREATED_STATUS, info.getStatus());
     }
 
     @Test
-    @DisplayName("Позитивная регистрация пользователя")
+    @Tag("negativeUserTest")
+    @DisplayName("Негативная регистрация пользователя")
     public void negativeRegisterUser() {
+
         int randomNumber = Math.abs(random.nextInt());
         User user = User.builder().login("alex" + randomNumber).pass("human" + randomNumber).build();
+
         given().contentType(ContentType.JSON)
                 .body(user)
                 .post("/api/signup")
                 .then()
                 .statusCode(201);
+
         Info info = given().contentType(ContentType.JSON)
                 .body(user)
                 .post("/api/signup")
@@ -52,15 +62,19 @@ public class ApiTest extends BaseApiTest {
                 .extract()
                 .jsonPath()
                 .getObject("info", Info.class);
+
         assertEquals(USER_FAIL_CREATED_MESSAGE, info.getMessage());
         assertEquals(USER_FAIL_CREATED_STATUS, info.getStatus());
     }
 
     @Test
+    @Tag("positiveUserTest")
     @DisplayName("Позитивный авторизация нового пользователя")
-    public void positiveAuth(){
+    public void positiveAuth() {
+
         int randomNumber = Math.abs(random.nextInt(10000));
         User user = User.builder().login("alex" + randomNumber).pass("human" + randomNumber).build();
+
         given().contentType(ContentType.JSON)
                 .body(user)
                 .post("/api/signup")
@@ -77,14 +91,18 @@ public class ApiTest extends BaseApiTest {
                 .extract()
                 .jsonPath()
                 .getString("token");
+
         Assertions.assertNotNull(token);
     }
 
     @Test
+    @Tag("negativeUserTest")
     @DisplayName("Негативный тест на авторизацию несуществующего пользователя")
-    public void negativeAuthTest(){
+    public void negativeAuthTest() {
+
         int randomNumber = Math.abs(random.nextInt(10000));
         JwtAuth jwtAuth = new JwtAuth("pass" + randomNumber, "login" + randomNumber);
+
         ErrorInfoAuth errorInfoAuth = given().contentType(ContentType.JSON)
                 .body(jwtAuth)
                 .post("/api/login")
@@ -92,7 +110,45 @@ public class ApiTest extends BaseApiTest {
                 .statusCode(401)
                 .extract()
                 .as(ErrorInfoAuth.class);
+
         Assertions.assertEquals(ERROR_UNAUTHORIZED, errorInfoAuth.getError());
+    }
+
+    @Test
+    @Tag("positiveUserTest")
+    @DisplayName("Позитивный тест на получение данных пользователя по токену")
+    public void postitveGetUser() {
+
+        int randomNumber = Math.abs(random.nextInt(10000));
+        User user = User.builder().login("alex" + randomNumber).pass("human" + randomNumber).build();
+
+        given().contentType(ContentType.JSON)
+                .body(user)
+                .post("/api/signup")
+                .then()
+                .statusCode(201);
+
+        JwtAuth jwtAuth = new JwtAuth(user.getPass(), user.getLogin());
+
+        String token = given().contentType(ContentType.JSON)
+                .body(jwtAuth)
+                .post("/api/login")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getString("token");
+
+        User userInfo = given()
+                .auth()
+                .oauth2(token)
+                .get("/api/user")
+                .then()
+                .extract()
+                .as(User.class);
+
+        Assertions.assertEquals(user.getLogin(), userInfo.getLogin());
+        Assertions.assertEquals(user.getPass(), userInfo.getPass());
     }
 
 }
